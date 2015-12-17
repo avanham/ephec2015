055dc2c3574f7cb33895e5ae17b761988bbdb16a
alex <alexandre.vanham.mail@gmail.com>
2015-12-17 21:25:43 +0100

    Bataille Navale

--- src/be/ephec/batNav/MainFrame.java
+++ src/be/ephec/batNav/MainFrame.java
@@ -13,7 +13,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -21,23 +20,21 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import be.ephec.serveur.MonServeur;
-
 public class MainFrame extends JFrame implements MouseListener, ActionListener{
 	private boolean jeu = false;
 	private JPanel contentPane;
 	private String[][] listeButtons = new String[Param.TAILLE_GRILLE+1][Param.TAILLE_GRILLE]; // +1 pour indiquer le joueur
-	protected int batSelected;
-	protected ImagePanel[] bat = new ImagePanel[3];
-	protected ImagePanel[][] touch = new ImagePanel[8][8];
+	protected int batSelected; // pour le bateau sélectionné
+	protected ImagePanel[] bat = new ImagePanel[3]; // pour les images des 3 bateaux
+	protected ImagePanel[][] touch = new ImagePanel[8][8]; 
 	protected ImagePanel[][] eau = new ImagePanel[8][8];
 	protected ImagePanel[][] touchAdv = new ImagePanel[8][8];
 	protected ImagePanel[][] eauAdv = new ImagePanel[8][8];
 	protected ImagePanel grilleBas;
 	protected ImagePanel grilleTop;
 	protected JLabel jLabMess;
-	protected int listBatPlacee[] = new int[6];
+	protected int listBatPlacee[] = new int[6]; // liste provisoire, pour placer les bateaux (cache)
 	private JButton btnDmarServ;
 	protected int port;
 	private JButton btnConnecterAuServeur;
@@ -47,7 +44,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 	private ObjectOutputStream oos;
 	private int player = 1;
 	private Calcul c;
-	private String lett = "ABCDEFGH";
+	private String lett = "ABCDEFGH"; // pour indiquer la ligne de la grille
 	private int x;
 	private int y;
 	private boolean tour;
@@ -87,8 +84,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 		Image[] imges = new Image[3]; // images des bateaux
 		Image imgEau = null;
 		Image imgTouch = null;
-
-		try {
+		try { //importe les images
 			img = ImageIO.read(new File("./img/grille.jpg"));
 			imges[0] = ImageIO.read(new File("./img/b2.jpg"));
 			imges[1] = ImageIO.read(new File("./img/b3.jpg"));
@@ -99,6 +95,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 			e.printStackTrace();
 		}
 		
+		// une boucle qui crée tout les croix et points qui vont être visible quand on clicke dans une case pendant la bataille
 		for(int i=0; i<8; i++){
 			for(int a=0; a<8; a++){
 				int coordX = c.getCaseX(i);
@@ -123,6 +120,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 			}
 		}
 		
+		//création des 3 bateaux
 		for(int i=0; i<3;i++){
 			bat[i] = new ImagePanel(imges[i]);
 			bat[i].setBounds(357, 411+(i*40), 66+(i*33), 34);
@@ -173,19 +171,22 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 		btnConnecterAuServeur.setBounds(357, 163, 145, 34);
 		contentPane.add(btnConnecterAuServeur);
 	}
+	/**
+	 * Prend en charge les actions qu'il faut faire quand-on click sur un bateau ou dans une grille
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		int elem = e.getComponent().getSize().width; // pas très propre, mais permet d'identifier la frame
 		if(elem == 300){ // les grilles ont une largeur de 300 px
 			this.x = this.c.getClickX(e.getX());
 			this.y = this.c.getClickY(e.getY());
-			if(jeu){
+			if(jeu){ // quand le jeu a commencé:
 				try {
-					if(tour){
-						if(this.listeButtons[y][x] == null){
-							this.oos.writeObject(x+""+y);
+					if(tour){ // si c'est à son tour
+						if(this.listeButtons[y][x] == null){ // vérifie, si on a pas déja clické sur la case
+							this.oos.writeObject(x+""+y); // envoit le x et y de la case
 							this.jLabMess.setText("Vous avez tiré sur "+this.lett.charAt(y)+" "+(x+1));
-							this.tour = false;
+							this.tour = false; // pour finir son tour
 						}
 						else{
 							this.jLabMess.setText("Vous avez déja tiré sur "+this.lett.charAt(y)+" "+(x+1));
@@ -196,19 +197,19 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 				}catch (IOException e2) {
 					e2.printStackTrace();
 				}
-			}else{
+			}else{ // sinon on place les bateaux
 				int coordX = this.c.getCaseX(x);
 				int coordY = this.c.getCaseY(y);
-				if ( x>=0 && x<=6-this.batSelected && y>=0 && y<=7){
-					this.bat[this.batSelected].setBounds((int) coordX + Param.DEBUT_X_GRILLE_BAS,(int) coordY + Param.DEBUT_Y_GRILLE_BAS, (this.batSelected*33)+66, 34);
-					this.listBatPlacee[2*this.batSelected] = x;
+				if ( x>=0 && x<=6-this.batSelected && y>=0 && y<=7){ // vérifie que les bateaux ne sortent pas de la grille
+					this.bat[this.batSelected].setBounds((int) coordX + Param.DEBUT_X_GRILLE_BAS,(int) coordY + Param.DEBUT_Y_GRILLE_BAS, (this.batSelected*33)+66, 34); //les place dans la grille
+					this.listBatPlacee[2*this.batSelected] = x; // les mets dans la liste provisoire, pour si jamais il y a un changement
 					this.listBatPlacee[(2*this.batSelected)+1] = y;
 				}else{
 					this.jLabMess.setText("Vous êtes sorti de la grille");
 				}
 			}
 		}else { // sinon c'est un bateau
-			batSelected = (elem-66)/33;
+			batSelected = (elem-66)/33; // les bateaux ont une taille que se différencient de 33 pixels
 		}
 	}
 	@Override
@@ -225,7 +226,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 	}
 	
 	/**
-	 * 
+	 * Prend en charge les actions qu'il faut faire quand-on click sur les bouttons du menu
 	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
@@ -247,6 +248,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 			this.grilleTop.addMouseListener(this);
 			this.jeu = true;
 			
+			// place les bateaux dans la liste principale, pour l'envoyer au serveur
 			for(int i=0; i<2; i++){
 				this.listeButtons[this.listBatPlacee[0]+i][this.listBatPlacee[1]] = "bateau2";
 			}
@@ -270,7 +272,6 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 			this.jLabMess.setText("Attendez le 2ème joueur");
 			break;
 		case "nPart":
-			System.out.println("nouvelle Partie");
 			this.listeButtons = new String[Param.TAILLE_GRILLE+1][Param.TAILLE_GRILLE];
 			this.grilleBas.addMouseListener(this);
 			this.grilleTop.removeMouseListener(this);
@@ -303,7 +304,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 			this.port = Integer.parseInt((String) JOptionPane.showInputDialog(this, "Numéro de port :", "Serveur", JOptionPane.QUESTION_MESSAGE, null, null, Param.NUMPORTDEBASE));
 			try {
 				this.connectServer(this.ip, this.port);
-				player = (int) this.ois.readObject();
+				player = (int) this.ois.readObject(); // reçoit directement son numéro de joueur
 				this.setTitle("Joueur n°"+player);
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
@@ -324,10 +325,9 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 	}
 	
 	/**
-	 * 
-	 * @param numPort numéro de port de base = 62900
-	 * @throws IOException
-	 * @throws ClassNotFoundException
+	 * @param numPort est le numéro de port avec lequel le serveur va établir la connexion
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
 	 */
 	public void creeServeur(final int numPort) throws IOException, ClassNotFoundException{
 		Runnable r = new Runnable() {
@@ -347,8 +347,8 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 	}
 	
 	/**
-	 * @param ip 
-	 * @param port numéro de port de base = 62900
+	 * @param ip numéro d'ip avec lequel on se connecte au serveur
+	 * @param port numéro de port avec lequel on se connecte au serveur
 	 * @throws UnknownHostException
 	 * @throws IOException
 	 */
@@ -359,14 +359,29 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 		this.oos.writeObject("Connexion");
 	}
 	
+	/**
+	 * 
+	 * @return renvoi tout ce qu'il reçoit du input stream
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
 	public Object lire() throws ClassNotFoundException, IOException{
 		return ois.readObject();
 	}
 	
+	/**
+	 * 
+	 * @param text place le texte dans le jLabel de la fenêtre principale
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
 	public void ecrire(String text) throws ClassNotFoundException, IOException{
 		this.jLabMess.setText(text);
 	}
 	
+	/**
+	 * crée un nouveau Thread dans lequel il écoute le serveur et traite les signaux comme demandé
+	 */
 	public void ecouter(){
 		Runnable r = new Runnable() {
 	         public void run() {
@@ -378,7 +393,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 						 }else if(retour.substring(0, 1).equalsIgnoreCase("1")){ // 1 -> message de bataille
 							 ecrire("Vous avez tiré"+retour.substring(1));
 							 listeButtons[y][x] = retour.substring(1);
-							 if(retour.substring(2, 3).equalsIgnoreCase("s")){
+							 if(retour.substring(2, 3).equalsIgnoreCase("s")){ // s -> sur un bateau -> X
 								 touch[x][y].setVisible(true);
 								 nbTouch++;
 								 if (nbTouch == 9){
@@ -386,15 +401,15 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener{
 									 ecrire("Vous avez gagné");
 									 s.close();
 								 }
-							 }else if(retour.substring(2, 3).equalsIgnoreCase("d")){
+							 }else if(retour.substring(2, 3).equalsIgnoreCase("d")){ //d -> dans l'eau -> O
 								 eau[x][y].setVisible(true);
 							 }
 						 }else if(retour.substring(0, 1).equalsIgnoreCase("2")){ // 2 -> message de tour
 							 ecrire(retour.substring(1));
 							 tour = true;
-						 }else if(retour.substring(0, 1).equalsIgnoreCase("3")){ 
+						 }else if(retour.substring(0, 1).equalsIgnoreCase("3")){ // 3 -> l'adversaire a tiré dans l'eau
 							 eauAdv[Integer.parseInt(retour.substring(1, 2))][Integer.parseInt(retour.substring(2, 3))].setVisible(true);
-						 }else if(retour.substring(0, 1).equalsIgnoreCase("4")){ 
+						 }else if(retour.substring(0, 1).equalsIgnoreCase("4")){ // 4 -> l'adversaire a tiré sur un bateau
 							 touchAdv[Integer.parseInt(retour.substring(1, 2))][Integer.parseInt(retour.substring(2, 3))].setVisible(true);
 							 nbTouchAdv++;
 							 if (nbTouchAdv == 9){
